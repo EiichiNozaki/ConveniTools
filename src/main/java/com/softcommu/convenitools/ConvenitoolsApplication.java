05d7a61b46e29d451b80c3659c5c58ee7c633aa7
unknown <eiichi.nozaki@gmail.com>
2026-06-18 14:10:23 +0200

    first commit

---
+++ src/main/java/com/softcommu/convenitools/ConvenitoolsApplication.java
@@ -0,0 +1,13 @@
+package com.softcommu.convenitools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenitoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvenitoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcommu/convenitools/ConvenitoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcommu.convenitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenitoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
